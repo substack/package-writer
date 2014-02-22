bf16b0879ecb7a2972a391bd5387aebda9cf632d
Oleg Seletsky <oleg.b.seletsky@gmail.com>
2014-02-21 17:38:50 -0800

    make package-writer async

--- index.js
+++ index.js
@@ -2,37 +2,60 @@ var combine = require( "stream-combiner" );
 var path = require( "path" );
 var fs = require( "fs" );
 var resolve = require( "resolve" );
+var async = require( "async" );
+var mkdirp = require( "mkdirp" );
 
-module.exports = function( packageJSON, assets, outputDir ) {
+module.exports = function( packageJSON, assets, outputDir, callback ) {
 	var outputStreams = {};
 
 	packageJSON.cartero = packageJSON.cartero || {};
 
-	Object.keys( assets ).forEach( function( assetType ) {
-		assets[ assetType ].forEach( function( file ) {
-			var transforms = packageJSON.cartero[ assetType + "Transform" ] || [];
+	async.each( Object.keys( assets ), function( assetType, callback ) {
+		var transforms = packageJSON.cartero[ assetType + "Transform" ] || [];
 
+		async.each( assets[ assetType ], function( file, callback ) {
 			var outputStream = fs.createReadStream( file );
 
-			if( transforms.length !== 0 ) {
-				outputStream = outputStream.pipe( combine.apply( null, transforms.map( function( t ) {
-					return require( resolve.sync( t, { basedir : packageJSON.path } ) )( file );
-				} ) ) );
-			}
+			async.map( transforms, function( t, callback ) {
+				resolve( t, { basedir : packageJSON.path }, function( err, res ) {
+					if( err )
+						return callback( err );
+					console.log( res );
+					callback( null, require( res ) );
+				} );
+			}, function( err, results ) {
+				if( err )
+					return callback( err );
 
-			outputStreams[ file ] = outputStream;
+				var outputFile = path.join( outputDir, path.relative( packageJSON.path, file ) );
 
-			var outputFile = path.join( outputDir, path.relative( packageJSON.path, file ) );
-			if( assetType === "style" ) {
-				outputFile = renameFileExtension( outputFile, ".css" );
-			}
+				mkdirp( path.dirname( outputFile ), function( err ) {
+					if( err )
+						return callback( err );
 
-			outputStream.pipe( fs.createWriteStream( outputFile ) );
+					if( results.length !== 0 )
+						outputStream = outputStream.pipe( combine.apply( null, results.map( function( t ) {
+							return t( file );
+						} ) ) );
 
+					
+					if( assetType === "style" ) {
+						outputFile = renameFileExtension( outputFile, ".css" );
+					}
+
+					outputStream.pipe( fs.createWriteStream( outputFile ) );
+					outputStreams[ file ] = outputStream;
+					callback();
+				} );
+			} );
+
+		}, function( err ) {
+			callback( err );
 		} );
-	} );
 
-	return outputStreams;
+	}, function( err ) {
+		return callback( err, outputStreams );
+	} );
 };
 
 function renameFileExtension( file, toExt ) {
